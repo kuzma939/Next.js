4a67091f4a8d59a34590fe0207f602967498bb85
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-12-08 19:35:16 +0100

    jh

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+import StatusLabel, {Status} from "./components/status-labes";
+
+
+
+export default function Home() {
+  return (
+    <main>
+    <h1 className="text-xl">Home page</h1>
+    <StatusLabel status={Status.Active}>Active</StatusLabel>
+    <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+    <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+    <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </main>
+   
+);
+}
+
+ 
+
